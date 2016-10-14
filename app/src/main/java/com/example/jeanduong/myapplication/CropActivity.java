eeb942f41cc05bcbcdd4d802be77ff9b2cbace01
jeanduong <jean.duong@gmail.com>
2016-10-14 09:47:15 +0200

    Rotation tests

--- app/src/main/java/com/example/jeanduong/myapplication/CropActivity.java
+++ app/src/main/java/com/example/jeanduong/myapplication/CropActivity.java
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.graphics.Matrix;
 
 public class CropActivity extends AppCompatActivity {
 
@@ -16,8 +17,16 @@ public class CropActivity extends AppCompatActivity {
         ImageView snapshot_layer = (ImageView) findViewById(R.id.display_view);
         DragView drag_layer = (DragView) findViewById(R.id.drag_view);
 
-        snapshot_layer.setImageBitmap(BitmapFactory.decodeFile(MainActivity.SNAPSHOT_FILE_NAME));
+        //snapshot_layer.setImageBitmap(BitmapFactory.decodeFile(MainActivity.SNAPSHOT_FILE_NAME));
 
+        Bitmap bm = BitmapFactory.decodeFile(MainActivity.SNAPSHOT_FILE_NAME);
+
+        Matrix mt = new Matrix();
+        mt.postRotate(90);
+
+        bm = Bitmap.createBitmap(bm , 0, 0, bm.getWidth(), bm.getHeight(), mt, true);
+
+        snapshot_layer.setImageBitmap(bm);
     }
 
 }
